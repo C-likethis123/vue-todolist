68054f345654bdd3ba26e8845a7da9288d1bca34
Chow Jia Ying <chowjiaying211@gmail.com>
2019-12-29 23:04:12 +0800

    Adds functionality to modals
    Previously, the modals could not be closed, and data
    could not be updated. This commit allows the modal
    to be closed when the 'Cancel' button is clicked,
    and the 'Add Task' button will update the data.

---
+++ src/views/AddTask.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="ui modal">
+  <div class="header">Header</div>
+  <div class="content">
+    <p></p>
+  </div>
+  <div class="actions">
+    <div class="ui approve button">Approve</div>
+    <div class="ui button">Neutral</div>
+    <div class="ui cancel button">Cancel</div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+// import Messages from '@/components/Messages.vue'; // @ is an alias to /src
+import NavBar from '@/components/NavBar.vue';
+
+@Component({
+  components: {
+    NavBar
+  },
+})
+export default class AddTask extends Vue {
+
+}
+</script>
