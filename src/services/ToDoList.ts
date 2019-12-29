b5f74927d94f44bb049e192af8b4dcd6540d3b9e
Chow Jia Ying <chowjiaying211@gmail.com>
2019-12-29 22:45:14 +0800

    Adds a new service to access todo list data

---
+++ src/services/ToDoList.ts
@@ -0,0 +1,53 @@
+import {IToDo, IToDoList} from '@/components/IToDoListDisplay';
+
+const todosContentsDailies: IToDo[] = [
+    {
+      task: "Do something",
+      isCompleted: false
+    },
+    {
+      task: "Do something else",
+      isCompleted: false
+    },
+    {
+      task: "Very long task to test if rendering will stay the same",
+      isCompleted: false
+    }
+  ];
+
+const todosContentsOneOff = [
+    {
+      task: "Do some homework",
+      isCompleted: false
+    },
+    {
+      task: "Do something",
+      isCompleted: false
+    },
+    {
+      task: "Buy something",
+      isCompleted: false
+    },
+  ];
+
+export const todolists: IToDoList[] = [
+    { title: 'Dailies', contents: todosContentsDailies },
+    { title: 'One off tasks', contents: todosContentsOneOff },
+  ];
+
+export function addTask(listChosen: string, taskToAdd: string) {
+    let todolist: IToDoList;
+
+    if (listChosen === 'dailies') {
+        todolist = todolists[0].contents;
+    } else {
+        todolist = todolists[1].contents;
+    }
+
+    console.log(todolist);
+
+    todolist.push({
+        task: taskToAdd,
+        isCompleted: false,
+    });
+}
