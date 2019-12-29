b94a7cb24cc472a98fb69f9516b7fd4cc1fcaa1f
Chow Jia Ying <chowjiaying211@gmail.com>
2019-12-30 01:02:29 +0800

    Adds a menubar to the ToDo list title

---
+++ src/components/ToDoTitle.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="todo-list-title">
+    <div class="title">{{title}}</div>
+    <div class="ui secondary pointing menu" >
+      <a class="item" @click="toggleActive">Done</a>
+      <a class="item active" @click="toggleActive">Not done</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+import ToDo from "@/components/ToDo.vue";
+import IToDoList from "@/components/ToDoListDisplay.vue";
+
+@Component({})
+export default class ToDoTitle extends Vue {
+  @Prop(String) readonly title!: String;
+
+  toggleActive(event: Event, isCompleted: boolean) {
+    const elementClicked = event.target;
+    const elementParent = elementClicked.parentNode;
+
+    for (const childNode of elementParent.children) {
+      if (childNode === elementClicked) {
+        childNode.classList.add("active");
+      } else {
+        childNode.classList.remove("active");
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.todo-list-title {
+  background-color: #7dd386;
+  font-weight: bold;
+  /* box-sizing: border-box; */
+  font-size: 2em;
+  display: flex;
+  justify-content: space-between;
+  /* padding: 2%; */
+}
+</style>
